9c437ab8d27397c7ef4d6c616d93ec40cf6f2c63
Cornel Cristian Filip <filipcornelcristian@gmail.com>
2024-11-12 07:16:41 +0000

    Enhance logging in config.ts with color-coded output for network version and faucet host
    Signed-off-by: Cornel Cristian Filip <filipcornelcristian@gmail.com>

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,15 +10,18 @@ export const NETWORK = "devnet";
 
 // Get the fullnode URL for the network
 const version = getFullnodeUrl(NETWORK);
-console.log("--------------------------------");
+console.log("\x1b[32m--------------------------------");
 console.log(`'Sui Version:', ${version}`);
-console.log("--------------------------------");
+console.log("--------------------------------\x1b[0m");
 
 // Create a SuiClient instance pointing to the desired network
 export const suiClient = new SuiClient({ url: version });
 
 // Get the faucet host for the network
 export const faucetHost = getFaucetHost(NETWORK);
+console.log("\x1b[32m--------------------------------");
+console.log(`'Faucet Host:', ${faucetHost}`);
+console.log("--------------------------------\x1b[0m");
 
 // Define the gas budget for transactions
 export const GAS_BUDGET = 1000000000; // 1 SUI or 1000000000 MIST
